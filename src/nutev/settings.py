edb1c5ea995539105ebfffa91d1b8ed6dda57c2b
WillianVagner123 <wvagners@hotmail.com>
2026-05-17 18:52:40 -0300

    feat(export): add curated output directory to settings

--- src/nutev/settings.py
+++ src/nutev/settings.py
@@ -15,20 +15,21 @@ class NutevSettings:
 
     @property
     def output_dirs(self) -> dict[str, Path]:
-        b=self.project_root
+        b = self.project_root
         return {
-            "00_config": b/"00_config",
-            "01_querypacks": b/"01_querypacks",
-            "02_metadata": b/"02_metadata",
-            "03A_metadata_hits": b/"03_corpus"/"03A_metadata_hits",
-            "03B_public_downloads": b/"03_corpus"/"03B_public_downloads",
-            "03C_official_docs": b/"03_corpus"/"03C_official_docs",
-            "03D_manual_uploads": b/"03_corpus"/"03D_manual_uploads",
-            "04_ocr_text": b/"04_ocr_text",
-            "05_extraction": b/"05_extraction",
-            "06_tables": b/"06_tables",
-            "07_logs": b/"07_logs",
-            "08_docs": b/"08_docs",
+            "00_config": b / "00_config",
+            "01_querypacks": b / "01_querypacks",
+            "02_metadata": b / "02_metadata",
+            "03A_metadata_hits": b / "03_corpus" / "03A_metadata_hits",
+            "03B_public_downloads": b / "03_corpus" / "03B_public_downloads",
+            "03C_official_docs": b / "03_corpus" / "03C_official_docs",
+            "03D_manual_uploads": b / "03_corpus" / "03D_manual_uploads",
+            "04_ocr_text": b / "04_ocr_text",
+            "05_extraction": b / "05_extraction",
+            "06_tables": b / "06_tables",
+            "07_logs": b / "07_logs",
+            "08_docs": b / "08_docs",
+            "10_curated": b / "10_curated",
         }
 
 def load_json(path: Path) -> dict:
