65a1b9eba0c8489d176a1bc6e9ddb9cd600115e8
WillianVagner123 <wvagners@hotmail.com>
2026-05-29 14:09:17 -0300

    feat(search): load semantic query extensions

---
+++ src/nutev/querypacks/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from nutev.querypacks.semantic_extensions import apply_semantic_extensions
+
+apply_semantic_extensions()
+
+__all__ = ["apply_semantic_extensions"]
