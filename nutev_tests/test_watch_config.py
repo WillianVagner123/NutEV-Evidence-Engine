4af2550b2a2c9c2ee28d83cda9b22af8f4bda152
WillianVagner123 <wvagners@hotmail.com>
2026-06-05 23:07:47 -0300

    test(search): lock watch nutrition prescription terms

---
+++ nutev_tests/test_watch_config.py
@@ -0,0 +1,19 @@
+from nutev.global_watch.watch_config import WATCH_CATEGORIES
+
+
+def test_watch_categories_include_nutrition_prescription_terms() -> None:
+    expected_terms = {
+        "nutrition prescription",
+        "dietary prescription",
+        "food prescription",
+        "personalized meal planning",
+        "individualized meal plan",
+    }
+
+    for category in (
+        "lifestyle_medicine",
+        "diet_patterns",
+        "implementation_behavior",
+    ):
+        terms = {term.lower() for term in WATCH_CATEGORIES[category]}
+        assert expected_terms & terms
