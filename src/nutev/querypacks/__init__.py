12480a16ac75b8b9209dca89610651808c627e8c
WillianVagner123 <wvagners@hotmail.com>
2026-06-09 03:07:24 -0300

    feat(querypacks): apply therapeutic diet extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
+from nutev.querypacks.therapeutic_diet_extensions import apply_therapeutic_diet_extensions
 
 apply_semantic_extensions()
+apply_therapeutic_diet_extensions()
 
-__all__ = ["apply_semantic_extensions"]
+__all__ = ["apply_semantic_extensions", "apply_therapeutic_diet_extensions"]
