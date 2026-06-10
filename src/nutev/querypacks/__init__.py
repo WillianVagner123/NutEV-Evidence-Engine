06dfbf56b294bbb2b79f5aef9410fc65b41a701e
WillianVagner123 <wvagners@hotmail.com>
2026-06-10 03:07:36 -0300

    chore(querypacks): keep base semantic extensions first

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 from nutev.querypacks.nutrition_prescription_extensions import (
     apply_nutrition_prescription_extensions,
 )
-from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_nutrition_prescription_extensions()
