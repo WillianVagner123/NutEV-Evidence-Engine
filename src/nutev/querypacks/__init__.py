7df4b2e59af2e2b713e5f539eaaa7611113640c2
WillianVagner123 <wvagners@hotmail.com>
2026-06-10 07:06:22 -0300

    feat(querypacks): load food environment policy semantic extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from nutev.querypacks.food_environment_policy_extensions import (
+    apply_food_environment_policy_extensions,
+)
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
+apply_food_environment_policy_extensions()
 
-__all__ = ["apply_semantic_extensions"]
+__all__ = ["apply_food_environment_policy_extensions", "apply_semantic_extensions"]
