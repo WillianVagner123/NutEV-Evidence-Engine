5e9a450979a8bbf81f828c7b53555e41e7ca0694
WillianVagner123 <wvagners@hotmail.com>
2026-06-17 10:08:21 -0300

    feat(search): load hepatic steatosis query extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
+from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.hepatic_steatosis_extensions import apply_hepatic_steatosis_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
+apply_hepatic_steatosis_extensions()
+apply_food_access_benefit_extensions()
 
-__all__ = ["apply_semantic_extensions"]
+__all__ = [
+    "apply_food_access_benefit_extensions",
+    "apply_hepatic_steatosis_extensions",
+    "apply_semantic_extensions",
+]
