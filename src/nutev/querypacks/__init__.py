0a10b011b960eb3dc59a8404fca53ff8c68e7a17
WillianVagner123 <wvagners@hotmail.com>
2026-06-18 07:07:48 -0300

    feat(querypacks): apply provider mesh extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.provider_mesh_extensions import apply_provider_mesh_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_food_access_benefit_extensions()
+apply_provider_mesh_extensions()
 
-__all__ = ["apply_food_access_benefit_extensions", "apply_semantic_extensions"]
+__all__ = [
+    "apply_food_access_benefit_extensions",
+    "apply_provider_mesh_extensions",
+    "apply_semantic_extensions",
+]
