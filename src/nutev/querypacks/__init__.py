61cade4f33ac77b4eee5a7a4f97e10754bc44b81
WillianVagner123 <wvagners@hotmail.com>
2026-06-19 20:08:11 -0300

    feat(querypacks): load cardiometabolic diet quality extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
+from nutev.querypacks.diet_quality_extensions import apply_diet_quality_extensions
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_food_access_benefit_extensions()
+apply_diet_quality_extensions()
 
-__all__ = ["apply_food_access_benefit_extensions", "apply_semantic_extensions"]
+__all__ = [
+    "apply_diet_quality_extensions",
+    "apply_food_access_benefit_extensions",
+    "apply_semantic_extensions",
+]
