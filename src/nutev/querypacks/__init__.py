2aa68b40dca59b3473a7519ce4a150e9e2ac8cb2
WillianVagner123 <wvagners@hotmail.com>
2026-06-21 01:09:18 -0300

    feat(querypacks): enable processing quality extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.processing_quality_extensions import apply_processing_quality_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_food_access_benefit_extensions()
+apply_processing_quality_extensions()
 
-__all__ = ["apply_food_access_benefit_extensions", "apply_semantic_extensions"]
+__all__ = [
+    "apply_food_access_benefit_extensions",
+    "apply_processing_quality_extensions",
+    "apply_semantic_extensions",
+]
