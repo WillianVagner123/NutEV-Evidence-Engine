b3d832c26ec3a85a16f1fa56259cc8cab416eda1
WillianVagner123 <wvagners@hotmail.com>
2026-06-25 20:07:55 -0300

    feat(querypacks): apply social prescribing food referral extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -5,13 +5,18 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
+from nutev.querypacks.social_prescribing_extensions import (
+    apply_social_prescribing_food_referral_extensions,
+)
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_social_prescribing_food_referral_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
     "apply_semantic_extensions",
+    "apply_social_prescribing_food_referral_extensions",
 ]
