0c2cc5901b2edecf86d683e0d5f12097f5ed317a
WillianVagner123 <wvagners@hotmail.com>
2026-06-26 23:06:59 -0300

    feat(querypacks): load blood pressure nutrition extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from nutev.querypacks.blood_pressure_extensions import (
+    apply_blood_pressure_nutrition_extensions,
+)
 from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
@@ -9,8 +12,10 @@ from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_blood_pressure_nutrition_extensions()
 
 __all__ = [
+    "apply_blood_pressure_nutrition_extensions",
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
     "apply_semantic_extensions",
