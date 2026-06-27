1c9dc46f34abafe35d9629ecc5ae420c924516d5
WillianVagner123 <wvagners@hotmail.com>
2026-06-27 16:08:34 -0300

    feat(querypacks): register cardiometabolic diet pattern extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -3,15 +3,20 @@ from __future__ import annotations
 from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
+from nutev.querypacks.cardiometabolic_diet_pattern_extensions import (
+    apply_cardiometabolic_diet_pattern_extensions,
+)
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
+apply_cardiometabolic_diet_pattern_extensions()
 apply_food_access_benefit_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
+    "apply_cardiometabolic_diet_pattern_extensions",
     "apply_food_access_benefit_extensions",
     "apply_semantic_extensions",
 ]
