d36363f840e479d8ce109b2e3a498877192cdaeb
WillianVagner123 <wvagners@hotmail.com>
2026-06-29 15:06:32 -0300

    feat(querypacks): load sarcopenic obesity extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -4,14 +4,19 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.sarcopenic_obesity_extensions import (
+    apply_sarcopenic_obesity_extensions,
+)
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_sarcopenic_obesity_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
+    "apply_sarcopenic_obesity_extensions",
     "apply_semantic_extensions",
 ]
