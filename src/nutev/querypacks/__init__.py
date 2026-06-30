20de3823a75ad930a4c7a69e51e306e27cc8b0b2
WillianVagner123 <wvagners@hotmail.com>
2026-06-29 22:07:55 -0300

    feat(querypacks): apply group visit expansion

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -4,14 +4,17 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.group_visit_extensions import apply_group_visit_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_group_visit_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
+    "apply_group_visit_extensions",
     "apply_semantic_extensions",
 ]
