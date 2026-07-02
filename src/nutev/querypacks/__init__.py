a9d11a37d951d564a385b3c650725bbbd515a8ec
WillianVagner123 <wvagners@hotmail.com>
2026-07-02 06:08:23 -0300

    feat(querypacks): load nutrition prescription extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -4,14 +4,19 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.nutrition_prescription_extensions import (
+    apply_nutrition_prescription_extensions,
+)
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_nutrition_prescription_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
+    "apply_nutrition_prescription_extensions",
     "apply_semantic_extensions",
 ]
