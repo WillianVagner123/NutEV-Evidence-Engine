e34a64cbf19999755369bcf55943e8f85c3694de
WillianVagner123 <wvagners@hotmail.com>
2026-07-07 09:09:55 -0300

    feat(search): apply group visit watch extensions

--- src/nutev/global_watch/__init__.py
+++ src/nutev/global_watch/__init__.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from typing import Any
 
+from nutev.global_watch.group_visit_extensions import apply_group_visit_watch_extensions
 from nutev.global_watch.watch_extensions import apply_watch_taxonomy_extensions
 
 apply_watch_taxonomy_extensions()
+apply_group_visit_watch_extensions()
 
 __all__ = ["run_global_watch"]
 
