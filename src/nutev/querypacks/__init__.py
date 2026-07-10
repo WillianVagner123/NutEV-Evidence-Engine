791de48ab9555d26d76249bee9332f809277d737
WillianVagner123 <wvagners@hotmail.com>
2026-07-10 16:08:02 -0300

    feat(search): load remission nutrition query extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -4,14 +4,19 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.remission_nutrition_extensions import (
+    apply_remission_nutrition_extensions,
+)
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_remission_nutrition_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
+    "apply_remission_nutrition_extensions",
     "apply_semantic_extensions",
 ]
