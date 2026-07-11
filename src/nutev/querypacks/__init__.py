cb6ab3daa0d1d55a063dad71143f0606875a27b7
WillianVagner123 <wvagners@hotmail.com>
2026-07-10 23:09:06 -0300

    feat(querypacks): enable cardiorenal semantic extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
+from nutev.querypacks.cardiorenal_extensions import apply_cardiorenal_extensions
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
+apply_cardiorenal_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
 
 __all__ = [
     "apply_carbohydrate_quality_extensions",
+    "apply_cardiorenal_extensions",
     "apply_food_access_benefit_extensions",
     "apply_semantic_extensions",
 ]
