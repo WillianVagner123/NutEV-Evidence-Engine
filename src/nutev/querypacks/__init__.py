10308218ed142813f5ef5e6b7df0c5c70c66c6a6
WillianVagner123 <wvagners@hotmail.com>
2026-07-12 19:07:45 -0300

    feat(querypacks): enable cultural adaptation extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -6,6 +6,9 @@ from nutev.querypacks.adherence_engagement_extensions import (
 from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
+from nutev.querypacks.cultural_adaptation_extensions import (
+    apply_cultural_adaptation_extensions,
+)
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.group_visit_care_extensions import apply_group_visit_care_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
@@ -13,12 +16,14 @@ from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 apply_semantic_extensions()
 apply_adherence_engagement_extensions()
 apply_carbohydrate_quality_extensions()
+apply_cultural_adaptation_extensions()
 apply_food_access_benefit_extensions()
 apply_group_visit_care_extensions()
 
 __all__ = [
     "apply_adherence_engagement_extensions",
     "apply_carbohydrate_quality_extensions",
+    "apply_cultural_adaptation_extensions",
     "apply_food_access_benefit_extensions",
     "apply_group_visit_care_extensions",
     "apply_semantic_extensions",
