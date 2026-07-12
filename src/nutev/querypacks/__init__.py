793742821128410e6efe13fd1c885cb630d399b3
WillianVagner123 <wvagners@hotmail.com>
2026-07-12 07:09:38 -0300

    feat(querypacks): apply adherence engagement extension

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from nutev.querypacks.adherence_engagement_extensions import (
+    apply_adherence_engagement_extensions,
+)
 from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
@@ -7,10 +10,12 @@ from nutev.querypacks.food_access_extensions import apply_food_access_benefit_ex
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
+apply_adherence_engagement_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
 
 __all__ = [
+    "apply_adherence_engagement_extensions",
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
     "apply_semantic_extensions",
