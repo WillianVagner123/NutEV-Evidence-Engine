769580d9d0df143b2d5a0a074768b384db8db8db
WillianVagner123 <wvagners@hotmail.com>
2026-07-12 10:06:01 -0300

    feat(querypacks): register group visit care extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -7,16 +7,19 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
     apply_carbohydrate_quality_extensions,
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
+from nutev.querypacks.group_visit_care_extensions import apply_group_visit_care_extensions
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
 apply_adherence_engagement_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
+apply_group_visit_care_extensions()
 
 __all__ = [
     "apply_adherence_engagement_extensions",
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
+    "apply_group_visit_care_extensions",
     "apply_semantic_extensions",
 ]
