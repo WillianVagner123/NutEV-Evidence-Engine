a09261630b1c0b4fb54fd8de45c16555d38cdcd7
WillianVagner123 <wvagners@hotmail.com>
2026-07-13 08:10:36 -0300

    feat(querypacks): load iberoamerican guidance extensions

--- src/nutev/querypacks/__init__.py
+++ src/nutev/querypacks/__init__.py
@@ -8,6 +8,9 @@ from nutev.querypacks.carbohydrate_quality_extensions import (
 )
 from nutev.querypacks.food_access_extensions import apply_food_access_benefit_extensions
 from nutev.querypacks.group_visit_care_extensions import apply_group_visit_care_extensions
+from nutev.querypacks.iberoamerican_guidance_extensions import (
+    apply_iberoamerican_guidance_extensions,
+)
 from nutev.querypacks.semantic_extensions import apply_semantic_extensions
 
 apply_semantic_extensions()
@@ -15,11 +18,13 @@ apply_adherence_engagement_extensions()
 apply_carbohydrate_quality_extensions()
 apply_food_access_benefit_extensions()
 apply_group_visit_care_extensions()
+apply_iberoamerican_guidance_extensions()
 
 __all__ = [
     "apply_adherence_engagement_extensions",
     "apply_carbohydrate_quality_extensions",
     "apply_food_access_benefit_extensions",
     "apply_group_visit_care_extensions",
+    "apply_iberoamerican_guidance_extensions",
     "apply_semantic_extensions",
 ]
