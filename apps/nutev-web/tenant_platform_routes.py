c76df8d867a07646397ba243b7840f2036dd7fd6
WillianVagner123 <wvagners@hotmail.com>
2026-09-08 14:36:50 -0300

    feat(article2): install integrative workflow routes

--- apps/nutev-web/tenant_platform_routes.py
+++ apps/nutev-web/tenant_platform_routes.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from article1_d132_api import install_article1_d132_routes
+from article2_integrative_api import install_article2_integrative_routes
 from tenant_application_api import install_application_routes
 from tenant_library_api import install_library_routes
 
@@ -10,3 +11,4 @@ def install_tenant_platform_routes() -> None:
     install_library_routes()
     install_application_routes()
     install_article1_d132_routes()
+    install_article2_integrative_routes()
